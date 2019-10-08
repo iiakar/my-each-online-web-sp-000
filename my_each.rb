50e524f68fdf6cc65b7e5565da1d9ecb1aa8eb29
Ibrahim  Akar <ibsakar@gmail.com>
2019-10-08 10:49:06 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,5 +2,6 @@ def my_each(arguments) # put argument(s) her
   i = 0
   while i < array.length 
   item = array[i]
-  puts "The current array item is #{"
+  puts "The current array item is #{item}"
+  
 end
